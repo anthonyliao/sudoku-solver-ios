1e80df27638fac1bdcd9543b282f9a9713c72a5f
anthonyliao <alisforanthonyliao@gmail.com>
2014-10-30 01:50:42 -0400

    initial empty project comment. add xcglogger

---
+++ sudoku-solver-ios/sudoku-solver-ios/Classes/Controllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sudoku-solver-ios
+//
+//  Created by ANTHONY on 10/30/14.
+//  Copyright (c) 2014 ANTHONY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
